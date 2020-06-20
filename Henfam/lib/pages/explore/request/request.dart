40960ad24d33a77b0eb3c540dae3925ea60bf522
jrb485 <jrb485@cornell.edu>
2020-06-20 15:26:41 -0400

    added intl dependency

--- Henfam/lib/pages/explore/request/request.dart
+++ Henfam/lib/pages/explore/request/request.dart
@@ -6,10 +6,77 @@ class Request extends StatefulWidget {
 }
 
 class _RequestState extends State<Request> {
+  DateTime _date = DateTime.now();
+  TimeOfDay _time = TimeOfDay.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2021),
+    );
+
+    if (picked != null && picked != _date) {
+      setState(() {
+        _date = picked;
+      });
+    }
+  }
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: _time,
+    );
+
+    if (picked != null && picked != _time) {
+      setState(() {
+        _time = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hello!"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text(
+          'Your Request',
+          // style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Delivery Options",
+              style: TextStyle(fontSize: 18),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Select Date'),
+                  onPressed: () {
+                    _selectDate(context);
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Select Time'),
+                  onPressed: () {
+                    _selectTime(context);
+                  },
+                ),
+              ],
+            ),
+            Text('Delivery for ${_date.toString()} at ${_time.toString()}')
+          ],
+        ),
+      ),
     );
   }
 }
