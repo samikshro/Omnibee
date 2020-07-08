5b3035d60a76f72e961e823d09dce5b27ec790fb
Samik Shrotriya <ss2659@cornell.edu>
2020-07-08 08:25:45 -0700

    big mode initial version

--- Henfam/lib/pages/explore/big_explore/bigCard.dart
+++ Henfam/lib/pages/explore/big_explore/bigCard.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: EdgeInsets.all(10.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      elevation: 10.0,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           const ListTile(
-            leading: Icon(Icons.album),
+            leading: Icon(Icons.fastfood),
             title: Text('Oiishi Bowl'),
             subtitle: Text('Olin Library: Deliver food to Sandra'),
           ),
-          // Image.asset(
-            
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image(
+              image: AssetImage(
+                  "assets/oishii_bowl_pic1.png"), //document['small_photo']),
+              fit: BoxFit.cover,
+            ),
+          ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     color: const Color(0xff7c94b6),
+          //     image: DecorationImage(
+          //       image: AssetImage(
+          //           "assets/oishii_bowl_pic1.png"), //document['small_photo']),
+          //       fit: BoxFit.cover,
+          //     ),
+          //     border: Border.all(
+          //       color: Colors.black,
+          //       width: 8,
+          //     ),
+          //     borderRadius: BorderRadius.circular(12),
+          //   ),
           // ),
           ButtonBar(
             children: <Widget>[
--- Henfam/lib/pages/explore/big_explore/bigMode.dart
+++ Henfam/lib/pages/explore/big_explore/bigMode.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'bigCard.dart';
-import 'bigErrands.dart';
+import 'package:Henfam/widgets/largeTextSection.dart';
 
 class BigMode extends StatefulWidget {
   @override
@@ -8,75 +8,51 @@ class BigMode extends StatefulWidget {
 }
 
 class _BigModeState extends State<BigMode> {
-  bool isExpanded;
-
-  // @override
-  // void initState() {
-  //   this.isExpanded = false;
-  //   super.initState();
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
+        Padding(
+          padding: EdgeInsets.all(10),
+        ),
+        LargeTextSection('Run Errand'),
+        Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+              color: Colors.black26,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              )),
+          child: TextField(
+            decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.white54,
+                ),
+                hintText: "Search",
+                hintStyle: TextStyle(
+                  color: Colors.white54,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.tune,
+                    color: Colors.white54,
+                  ),
+                  tooltip: 'Filter',
+                  onPressed: () {
+                    print('hit filter');
+                  },
+                )),
+          ),
+        ),
         Expanded(
             child: ListView.builder(
                 itemCount: 5,
                 itemBuilder: (context, index) {
                   return BigCard();
-                }
-                // CurrentOrder("Jessie", "7:30", "on the way"),
-                // ScheduledRequests(),
-                // LargeTextSection("What would you like help with, John?"),
-                // Container(
-                //   child: HelpCard(activities),
-                // ),
-                // LargeTextSection("Or choose from other errands nearby!"),
-                // Padding(
-                //   padding: EdgeInsets.only(bottom: 60),
-                //   child: ErrandsNearby(),
-                // ),
-                ))
+                }))
       ]),
     );
-    //     body: Column(
-    //   children: <Widget>[
-    //     Expanded(
-    //       child: ListView.builder(
-    //         scrollDirection: Axis.horizontal,
-    //         itemCount: 2,
-    //         itemBuilder: (BuildContext context, int index) {
-    //           return Text('HHHHello');
-    //         },
-    //       ),
-    //     )
-    //   ],
-    // )
-    //     body: SingleChildScrollView(
-    //   scrollDirection: Axis.vertical,
-    //   child: Column(
-    //     crossAxisAlignment: CrossAxisAlignment.start,
-    //     children: <Widget>[
-    //       Expanded(
-    //         child: ExpansionPanelList(
-    //           expansionCallback: (int index, bool isExpanded) {
-    //             setState(() {
-    //               this.isExpanded = !isExpanded;
-    //             });
-    //           },
-    //           children: <ExpansionPanel>[
-    //             new ExpansionPanel(
-    //               headerBuilder: (BuildContext context, bool isExpanded) =>
-    //                   const Text("header data"),
-    //               body: const Text("Body data"),
-    //               isExpanded: this.isExpanded,
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // )
   }
 }
