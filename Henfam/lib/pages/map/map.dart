3e6a22d8fe672ad9a109a2ad433c18d543fb2d44
jrb485 <jrb485@cornell.edu>
2020-07-16 12:42:35 -0400

    deleted redundant map code and files

--- Henfam/lib/pages/map/map.dart
+++ Henfam/lib/pages/map/map.dart
@@ -1,5 +1,4 @@
 import 'package:Henfam/pages/map/mapArgs.dart';
-import 'package:Henfam/pages/map/positionModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -77,7 +76,7 @@ class _CustomMapState extends State<CustomMap> {
             onTap: () {
               Navigator.pushNamed(context, '/expanded_map',
                   arguments: MapArgs(
-                      _createMarkers2(
+                      _createMarkers(
                         snapshot.data,
                       ),
                       snapshot.data[1]));
@@ -91,7 +90,7 @@ class _CustomMapState extends State<CustomMap> {
                   child: GoogleMap(
                     myLocationButtonEnabled: false,
                     onMapCreated: _onMapCreated,
-                    markers: _createMarkers2(
+                    markers: _createMarkers(
                       snapshot.data,
                     ),
                     initialCameraPosition: CameraPosition(
@@ -134,7 +133,7 @@ class _CustomMapState extends State<CustomMap> {
   }
 }
 
-Set<Marker> _createMarkers2(List<Position> positions) {
+Set<Marker> _createMarkers(List<Position> positions) {
   var markers = <Marker>[];
 
   for (var i = 0; i < positions.length; i++) {
@@ -152,83 +151,4 @@ Set<Marker> _createMarkers2(List<Position> positions) {
   }
 
   return markers.toSet();
-}
-
-Set<Marker> _createMarkers(user_position, requesters, restaurant) {
-  final positions = PositionModel.mockPositionData;
-
-  List<Marker> requesterMarkers = _createRequesterMarkers(requesters);
-  List<Marker> userMarker = _createUserMarker(user_position);
-  List<Marker> restaurantMarker = _createRestaurantMarker(restaurant);
-
-  List<Marker> markers = [];
-  markers.addAll(requesterMarkers);
-  markers.addAll(userMarker);
-  markers.addAll(restaurantMarker);
-
-  return markers.toSet();
-}
-
-List<Marker> _createRestaurantMarker(restaurant) {
-  var markers = <Marker>[];
-  List<double> location = restaurant['location'];
-
-  markers.add(
-    Marker(
-      markerId: MarkerId(restaurant['name']),
-      position: LatLng(
-        location[0],
-        location[1],
-      ),
-      infoWindow: InfoWindow(title: restaurant['name']),
-      icon: BitmapDescriptor.defaultMarkerWithHue(
-        BitmapDescriptor.hueOrange,
-      ),
-    ),
-  );
-  return markers;
-}
-
-List<Marker> _createUserMarker(user_position) {
-  var markers = <Marker>[];
-
-  markers.add(
-    Marker(
-      markerId: MarkerId('User'),
-      position: LatLng(
-        user_position.latitude,
-        user_position.longitude,
-      ),
-      infoWindow: InfoWindow(title: 'User'),
-      icon: BitmapDescriptor.defaultMarkerWithHue(
-        BitmapDescriptor.hueOrange,
-      ),
-    ),
-  );
-
-  return markers;
-}
-
-List<Marker> _createRequesterMarkers(List<Map<String, Object>> requesters) {
-  var markers = <Marker>[];
-
-  for (var i = 0; i < requesters.length; i++) {
-    List<double> location = requesters[i]['location'];
-    if (requesters[i]['selected'] == true) {
-      final newMarker = Marker(
-        markerId: MarkerId(requesters[i]['name']),
-        position: LatLng(
-          location[0],
-          location[1],
-        ),
-        infoWindow: InfoWindow(title: requesters[i]['name']),
-        icon: BitmapDescriptor.defaultMarkerWithHue(
-          BitmapDescriptor.hueOrange,
-        ),
-      );
-
-      markers.add(newMarker);
-    }
-  }
-  return markers;
 }
--- Henfam/lib/pages/map/mapArgs.dart
+++ Henfam/lib/pages/map/mapArgs.dart
@@ -1,4 +1,3 @@
-import 'package:Henfam/pages/map/positionModel.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
