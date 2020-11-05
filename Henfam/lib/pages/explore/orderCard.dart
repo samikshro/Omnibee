74b3b66b623291d845837546603381e3a168f3b8
Jackson Booth <jacksonrbooth@hotmail.com>
2020-11-04 20:59:10 -0500

    changed price to new formula

--- Henfam/lib/pages/explore/orderCard.dart
+++ Henfam/lib/pages/explore/orderCard.dart
@@ -83,7 +83,9 @@ class OrderCardButtonBar extends StatelessWidget {
       content: Text('Confirming delivery, please wait one moment....'),
     );
     Scaffold.of(context).showSnackBar(snackBar);
-    PaymentService.paymentTransfer(doc, context, doc['user_id']['price'], 1.23,
+    double priceWithTax = 1.28 * doc['user_id']['price'];
+    double fees = (1.28 * doc['user_id']['price'] * .03) + .3;
+    PaymentService.paymentTransfer(doc, context, priceWithTax + fees, fees,
         doc['user_id']['payment_method_id'], doc['stripeAccountId']);
   }
 
