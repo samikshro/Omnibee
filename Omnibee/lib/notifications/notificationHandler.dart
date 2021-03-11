3da0901c00caa14795214f4d4f61245650704a3e
Samik Shrotriya <samikshro@gmail.com>
2021-03-11 17:15:29 -0500

    finished release

--- Omnibee/lib/notifications/notificationHandler.dart
+++ Omnibee/lib/notifications/notificationHandler.dart
@@ -50,10 +50,10 @@ class _NotificationHandlerState extends State<NotificationHandler> {
             builder: (context) {
               return AlertDialog(
                 content: ListTile(
-                  title: Text(message['notification']['title']),
-                  subtitle: Text(message['notification']['body']),
-                  /* title: Text(message['aps']['alert']['title']),
-                  subtitle: Text(message['aps']['alert']['body']), */
+                  // title: Text(message['notification']['title']),
+                  // subtitle: Text(message['notification']['body']),
+                  title: Text(message['aps']['alert']['title']),
+                  subtitle: Text(message['aps']['alert']['body']),
                 ),
                 actions: <Widget>[
                   FlatButton(
