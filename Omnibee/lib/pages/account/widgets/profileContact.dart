304eb797f6b2e66e4b8b1bf33fff270b7342472f
Samik Shrotriya <samikshro@gmail.com>
2021-03-16 16:56:02 -0400

    edited help desk info

--- Omnibee/lib/pages/account/widgets/profileContact.dart
+++ Omnibee/lib/pages/account/widgets/profileContact.dart
@@ -1,6 +1,8 @@
 import 'package:Omnibee/pages/account/widgets/customTile.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfileContact extends StatelessWidget {
   final Function signOut;
@@ -16,11 +18,29 @@ class ProfileContact extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
-                child: Text(
-                    'For errors and feedback, please contact us by phone or email:\n\nhello@omnibee.io\n(607)269-8901',
-                    style: TextStyle(fontSize: 18)),
-              ),
+                  padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
+                  child: RichText(
+                      text: TextSpan(children: [
+                    new TextSpan(
+                      text: 'This is no Link, ',
+                      style: new TextStyle(color: Colors.black),
+                    ),
+                    new TextSpan(
+                      text: 'but this is',
+                      style: new TextStyle(color: Colors.blue),
+                      recognizer: new TapGestureRecognizer()
+                        ..onTap = () {
+                          launch('omnibee.io/faq');
+                        },
+                    ),
+                  ])
+
+                      //  Text(
+                      //     "You can view a list of frequently asked questions <link to faq>here</link>. If you need more help, please contact us by phone or email. \n\nhello@omnibee.io\n(607)269-8901 (Urgent enquiries)",
+                      //     // 'For errors and feedback, please contact us by phone or email:\n\nhello@omnibee.io\n(607)269-8901',
+                      //     style: TextStyle(fontSize: 18)),
+
+                      )),
               Center(
                 child: CupertinoButton(
                     color: Theme.of(context).primaryColor,
